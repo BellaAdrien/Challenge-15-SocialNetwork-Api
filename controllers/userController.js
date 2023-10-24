673611ce59f5231d6b22d1b29b4598dff4bfb67c
BellaAdrien <Iadrien2020@fau.edu>
2023-10-24 19:40:35 -0400

    save16

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const Thought = require('../models/Thought')
 module.exports = {
   async getUsers(req, res) {
     try {
-      const users = await User.find();
+      const users = await User.find().populate("thoughts");
       res.json(users);
     } catch (err) {
       res.status(500).json(err);
